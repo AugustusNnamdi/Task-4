406712517c82ee84f84dcf67ef0526528f94622d
AugustusNnamdi <115026896+AugustusNnamdi@users.noreply.github.com>
2022-11-15 21:49:23 +0100

    tomcat working commit

--- main.tf
+++ main.tf
@@ -110,7 +110,27 @@ resource "azurerm_app_service" "as1" {
 
 
 
+#creating third webapp
 
+resource "azurerm_app_service" "as2" {
+  name                = "jk-tomcat"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
 
+  site_config {
+    linux_fx_version = "DOCKER|tomcat:latest"
+    always_on        = "true"
+  }
+  tags = {
+    image = "tomcat"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
 
 
--- output.tf
+++ output.tf
@@ -4,12 +4,18 @@
 
 output "url" {
   value       = azurerm_app_service.as[*].default_site_hostname
-  description = "The url of the web app."
+  description = "The url of the webapp nginx."
 }
 
 output "url1" {
   value       = azurerm_app_service.as1[*].default_site_hostname
   description = "The url of the webapp wordpress."
 }
+
+output "url2" {
+  value       = azurerm_app_service.as2[*].default_site_hostname
+  description = "The url of the webapp tomcat."
+}
+
 
 
